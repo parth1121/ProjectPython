5591c5f1a4780fd9ea25e9c5a2798fe9a9696701
parth <parthdarak11@gmail.com>
2023-04-13 18:51:14 +0530

    changes in urls

--- Application/urls.py
+++ Application/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app1/', include('App1.urls')),
+    path('test/', include('App1.urls')),
 ]
